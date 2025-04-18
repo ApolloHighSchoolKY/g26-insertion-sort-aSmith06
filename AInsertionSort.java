a1195e1feb51f987758f54f675eb24c3ce711257
aSmith06 <andrew.smith2@stu.daviess.kyschools.us>
2025-04-18 15:32:57 +0000

    Sorted in two ways

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
-    	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+    	Integer myNumbers[] = {9, 3, 8, 6, 2, 37};
+        System.out.println(Arrays.toString(myNumbers));
+        int tempVal = 0;
 
+        for(int i = 1; i<myNumbers.length; i++){
+            tempVal = myNumbers[i];
+            for(int pos = i-1; pos >= 0; pos--){
+                if(tempVal < myNumbers[pos]){
+                    myNumbers[pos+1] = myNumbers[pos];
+                    myNumbers[pos] = tempVal;
+                }
+            }
+            System.out.println(Arrays.toString(myNumbers));
+        }
+        System.out.println(Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -8,26 +9,40 @@ public class ALInsertionSort
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
+		System.out.println(Arrays.toString(myNumbers));
 
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int i = 1; i < myNumbers.length; i++){
 			//Check with each of the numbers in the sorted list
-
+			inserted = false;
+			for(int s = 0; s < sorted.size(); s++){
 				//If this number is less than one in the sorted list,
-				//insert it there
-
+				if(myNumbers[i] < sorted.get(s)){
+					//insert it there
+					sorted.add(s, myNumbers[i]);
+					inserted = true;
+					s = sorted.size();
+					//System.out.println(sorted);
+				}
+			}
 			//If it was not inserted, stick it on the end.
+			if(!inserted)
+				sorted.add(myNumbers[i]);
 
 
-		//End Loop for unsorted list
+	}//End Loop for unsorted list
 
 		System.out.println(sorted);
 
 		//Move the data back to the array
+		for(int o = 0; o < sorted.size(); o++){
+			myNumbers[o] = sorted.get(o);
+		}
 
 		//Print the contents of the array
+		System.out.println(Arrays.toString(myNumbers));
 
     }
 
